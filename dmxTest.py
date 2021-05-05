3c189a71737b22ee5f6978940ea3ccd20cbb481f
Sam Wolk <mail@samwolk.info>
2021-05-05 17:05:20 -0400

    update dmxtest

--- dmxTest.py
+++ dmxTest.py
@@ -1,13 +1,15 @@
 from DMXEnttecPro import Controller
 import time
+import random
 
 dmx = Controller('/dev/ttyUSB0',auto_submit=True)
+count = 0
+testChannel = 5
+while True:
+	dmx.set_channel(testChannel,count)
+	count = (count+1)%255
+	time.sleep(0.01)
 
-dmx.set_channel(512,0)
-
-time.sleep(1)
-
-dmx.set_channel(512,255)
 
 # dmx.set_all_channels(val)
 # dmx.clear_channels()
